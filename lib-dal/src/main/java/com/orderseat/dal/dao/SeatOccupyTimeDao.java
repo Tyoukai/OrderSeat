afdac7baa9ca0b852ac8ba3ba8592603bed6ea96
张珂为 <zhangkewei02@meituan.com>
2019-11-18 08:54:39 +0800

    添加注释

--- lib-dal/src/main/java/com/orderseat/dal/dao/SeatOccupyTimeDao.java
+++ lib-dal/src/main/java/com/orderseat/dal/dao/SeatOccupyTimeDao.java
@@ -3,6 +3,12 @@ package com.orderseat.dal.dao;
 import com.orderseat.common.dal.BaseDao;
 import com.orderseat.dal.bean.SeatOccupyTimeBean;
 
+/**
+ * @author: tyoukai
+ * @date: 2019-11-18
+ * @description: 占座的Dao
+ * @version: v1.0
+ */
 public interface SeatOccupyTimeDao extends BaseDao<SeatOccupyTimeBean> {
 
 }

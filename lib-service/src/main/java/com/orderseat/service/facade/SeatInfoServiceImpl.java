1c4a2b80295660f7a3be063251917f7d772daa86
张珂为 <zhang_k_w@163.com>
2019-11-23 22:21:34 +0800

    完成了RPC服务的实现

--- lib-service/src/main/java/com/orderseat/service/facade/SeatInfoServiceImpl.java
+++ lib-service/src/main/java/com/orderseat/service/facade/SeatInfoServiceImpl.java
@@ -1,9 +1,20 @@
 package com.orderseat.service.facade;
 
+import com.orderseat.common.utils.system.QueryParams;
+import com.orderseat.dal.model.SeatModel;
 import com.orderseat.facade.SeatInfoService;
+import com.orderseat.facade.dto.SeatInfoDto;
+import com.orderseat.facade.enums.ResultCodeEnum;
 import com.orderseat.facade.request.SeatInfoRequest;
 import com.orderseat.facade.response.SeatInfoResponse;
+import com.orderseat.service.repository.SeatInfoRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.thrift.TException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author: tyoukai
@@ -12,18 +23,94 @@ import org.apache.thrift.TException;
  * @version: v1.0
  */
 public class SeatInfoServiceImpl implements SeatInfoService.Iface {
+    @Autowired
+    private SeatInfoRepository seatInfoRepository;
+
     @Override
     public SeatInfoResponse search(SeatInfoRequest request) throws TException {
-        return null;
+        SeatInfoResponse seatInfoResponse = new SeatInfoResponse();
+        try {
+            SeatInfoDto seatInfoDto = request.getSeatInfoDto();
+            QueryParams params = assemblingParams(seatInfoDto);
+            List<SeatModel> seatModelList = seatInfoRepository.search(params);
+            List<SeatInfoDto> seatInfoDtoList = seatModelList.stream().map(this::convert2SeatInfoDto).collect(Collectors.toList());
+            seatInfoResponse.setSeatInfoDtoList(seatInfoDtoList);
+            seatInfoResponse.setResultCode(ResultCodeEnum.SUCCESS);
+            seatInfoResponse.setDesc("查询结果成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            seatInfoResponse.setResultCode(ResultCodeEnum.FAIL);
+            seatInfoResponse.setDesc("查询异常,请检查OrderSeat系统");
+        }
+        return seatInfoResponse;
     }
 
     @Override
     public SeatInfoResponse add(SeatInfoRequest request) throws TException {
-        return null;
+        SeatInfoResponse seatInfoResponse = new SeatInfoResponse();
+        try {
+            SeatInfoDto seatInfoDto = request.getSeatInfoDto();
+            SeatModel seatModel = this.convert2SeatModel(seatInfoDto);
+            seatInfoRepository.add(seatModel);
+            seatInfoResponse.setResultCode(ResultCodeEnum.SUCCESS);
+            seatInfoResponse.setDesc("添加成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            seatInfoResponse.setResultCode(ResultCodeEnum.FAIL);
+            seatInfoResponse.setDesc("添加异常,请检查OrderSeat系统");
+        }
+        return seatInfoResponse;
     }
 
     @Override
     public SeatInfoResponse deleteSeat(SeatInfoRequest request) throws TException {
-        return null;
+        SeatInfoResponse seatInfoResponse = new SeatInfoResponse();
+        try {
+            SeatInfoDto seatInfoDto = request.getSeatInfoDto();
+            String id = seatInfoDto.getId();
+            seatInfoRepository.deleteById(id);
+            seatInfoResponse.setResultCode(ResultCodeEnum.SUCCESS);
+            seatInfoResponse.setDesc("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            seatInfoResponse.setResultCode(ResultCodeEnum.FAIL);
+            seatInfoResponse.setDesc("删除异常,请检查OrderSeat系统");
+        }
+        return seatInfoResponse;
+    }
+
+    /**
+     * 组装参数
+     *
+     * @param seatInfoDto
+     * @return
+     */
+    private QueryParams assemblingParams(SeatInfoDto seatInfoDto) {
+        if (seatInfoDto == null) {
+            return null;
+        }
+        QueryParams params = QueryParams.create();
+        if (StringUtils.isNotBlank(seatInfoDto.getId())) {
+            params.add("id", seatInfoDto.getId());
+        } else if (StringUtils.isNotBlank(seatInfoDto.getBuildingId())) {
+            params.add("buildingId", seatInfoDto.getBuildingId());
+        } else if (StringUtils.isNotBlank(seatInfoDto.getFloorId())) {
+            params.add("floorId", seatInfoDto.getFloorId());
+        } else if (StringUtils.isNotBlank(seatInfoDto.getSeatType())) {
+            params.add("seatType", seatInfoDto.getSeatType());
+        }
+        return params;
+    }
+
+    private SeatInfoDto convert2SeatInfoDto(SeatModel seatModel) {
+        SeatInfoDto seatInfoDto = new SeatInfoDto();
+        BeanUtils.copyProperties(seatModel, seatInfoDto);
+        return seatInfoDto;
+    }
+
+    private  SeatModel convert2SeatModel(SeatInfoDto seatInfoDto) {
+        SeatModel seatModel = new SeatModel();
+        BeanUtils.copyProperties(seatInfoDto, seatModel);
+        return seatModel;
     }
 }

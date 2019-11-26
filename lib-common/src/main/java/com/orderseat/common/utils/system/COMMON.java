f39d50b3552c615097c02176fdf88457579314f7
张珂为 <zhang_k_w@163.com>
2019-11-26 08:53:47 +0800

    增加了常量

---
+++ lib-common/src/main/java/com/orderseat/common/utils/system/COMMON.java
@@ -0,0 +1,17 @@
+package com.orderseat.common.utils.system;
+
+/**
+ * @author: tyoukai
+ * @date: 2019-11-26 8:47
+ * @description: 常量类
+ * @version: v1.0
+ */
+public class COMMON {
+    public static String REDIS_LIST_NAME_PREFIX = "orderseat-seatnum-";
+
+    public static String ORDER_SEAT_RESPONSE_STATUS_SUCCESS = "0";
+
+    public static String ORDER_SEAT_RESPONSE_STATUS_FAIL = "1";
+
+    public static String ORDER_SEAT_RESPONSE_STATUS_WAIT = "2";
+}

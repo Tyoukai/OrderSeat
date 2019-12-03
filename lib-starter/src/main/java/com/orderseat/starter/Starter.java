06f15c7130028097eeaef1918e26c8f8d538ad68
张珂为 <zhang_k_w@163.com>
2019-12-03 20:36:37 +0800

    添加了启动类

---
+++ lib-starter/src/main/java/com/orderseat/starter/Starter.java
@@ -0,0 +1,15 @@
+package com.orderseat.starter;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author: tyoukai
+ * @date: 2019-12-03 20:33
+ * @description: 启动类
+ * @version: v1.0
+ */
+public class Starter {
+    public static void main(String[] args) {
+        new ClassPathXmlApplicationContext("orderseat-starter-config.xml");
+    }
+}

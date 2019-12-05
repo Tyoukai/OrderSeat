30b00b3f0db9ccd46d92bae8202f8519af75f992
张珂为 <zhang_k_w@163.com>
2019-12-05 08:13:59 +0800

    修改了随机数产生类

--- lib-common/src/main/java/com/orderseat/common/utils/system/Random.java
+++ lib-common/src/main/java/com/orderseat/common/utils/system/Random.java
@@ -1,5 +1,11 @@
 package com.orderseat.common.utils.system;
 
+/**
+ * @author: tyoukai
+ * @date: 2019-11-29 21:08
+ * @description: 随机类
+ * @version: v1.0
+ */
 public class Random {
 
     private static java.util.Random random = new java.util.Random();
